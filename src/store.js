6d84e413b168dc14527f6a4be5713ab56d967b5d
Harshit Singh <singhjdk299288@gmail.com>
2023-06-08 18:21:58 +0530

    bug fix

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { legacy_createStore as createStore  } from 'redux';
-import {  combineReducers } from 'redux';
+import {  combineReducers,applyMiddleware} from 'redux';
+import logger from 'redux-logger';
 import reducerWeekly from './components/home/reduxWeekly/reducer';
 import reducer from './components/home/posts/reduxPost/reducer';
 
+const middleware = [logger];
 // Combine reducers
 const rootReducer = combineReducers({
   daysreducer: reducerWeekly,
@@ -10,6 +12,6 @@ const rootReducer = combineReducers({
 });
 
 // Create the Redux store
-const store = createStore(rootReducer);
+const store = createStore(rootReducer,applyMiddleware(...middleware));
 
 export default store;
