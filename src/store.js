5e87aae6af09821edf290d4b96db2ee6578edc19
Harshit Singh <singhjdk299288@gmail.com>
2023-06-09 10:08:33 +0530

    Final Push

--- src/store.js
+++ src/store.js
@@ -5,13 +5,13 @@ import reducerWeekly from './components/home/reduxWeekly/reducer';
 import reducer from './components/home/posts/reduxPost/reducer';
 
 const middleware = [logger];
-// Combine reducers
+
 const rootReducer = combineReducers({
   daysreducer: reducerWeekly,
   posts: reducer
 });
 
-// Create the Redux store
+
 const store = createStore(rootReducer,applyMiddleware(...middleware));
 
 export default store;
